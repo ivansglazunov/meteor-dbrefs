5dfcf6aa23e0cf6ee001d8aeb408662d1ff16719
ivansglazunov <ivansglazunov@gmail.com>
2015-12-20 00:44:44 +0300

    Basic file structure.

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'ivansglazunov:dbrefs',
+  version: '0.0.1',
+  summary: 'Lovely tools to work with DBRefs in Meteor.',
+  git: 'https://github.com/ivansglazunov/meteor-dbrefs',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('dbrefs.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('ivansglazunov:dbrefs');
+  api.addFiles('tests.js');
+});
