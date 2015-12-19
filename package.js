1ffda7347dbc6bb165315ef239b4b01e92904ea3
ivansglazunov <ivansglazunov@gmail.com>
2015-12-20 02:20:03 +0300

    0.0.1

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('mongo');
-  api.use('dburles:mongo-collection-instances');
+  api.use('dburles:mongo-collection-instances@0.3.4');
   api.use('ecmascript');
   api.addFiles('dbrefs.js');
 });
